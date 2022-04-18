86a789a71565f320f75570660ab27994fa5940ac
Michal <mwargan@gmail.com>
2022-04-18 13:52:55 +0000

    Reduced food item height

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -28,7 +28,7 @@ export default defineComponent({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 img {
-  height: 70vh;
+  height: 60vh;
   object-fit: scale-down;
   max-width: 100%;
 }
