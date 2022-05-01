a089e7df356a08e12197e2aef80a7b2db4971331
Michal <mwargan@gmail.com>
2022-05-01 13:54:08 +0200

    Added info about installing app

---
+++ src/components/InstallApp.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="grid" v-if="!isRunningInStandalone">
+    <p v-if="currentBrowser === 'Safari'">
+      Install this app on iOS by tapping the
+      <a href.prevent="#" @click="openShare()">"Share"</a> icon, then choose
+      "Add to homescreen".
+    </p>
+    <p v-else>
+      Follow the native prompts provided by your device to install this app
+    </p>
+    <p>
+      Installing this app will make sure that all your personal data is stored
+      securely on your device.
+    </p>
+  </div>
+  <div v-else class="grid">
+    <p>Looks like you've already installed the app :)!</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InstallApp",
+  computed: {
+    isRunningInStandalone() {
+      return (
+        (window.navigator as any).standalone === true ||
+        window.matchMedia("(display-mode: standalone)").matches
+      );
+    },
+    currentBrowser() {
+      let userAgent = navigator.userAgent;
+      let browserName;
+
+      if (userAgent.match(/chrome|chromium|crios/i)) {
+        browserName = "Chrome";
+      } else if (userAgent.match(/firefox|fxios/i)) {
+        browserName = "Firefox";
+      } else if (userAgent.match(/safari/i)) {
+        browserName = "Safari";
+      } else if (userAgent.match(/opr\//i)) {
+        browserName = "Opera";
+      } else if (userAgent.match(/edg/i)) {
+        browserName = "Edge";
+      } else {
+        browserName = "No browser detection";
+      }
+      return browserName;
+    },
+  },
+  methods: {
+    async openShare() {
+      if ((navigator as any).canShare) {
+        navigator
+          .share({
+            title: "Eat.Ninja",
+            text: "Eat Fresh, Eat Healthy!",
+            url: "https://eat.ninja",
+          })
+          .then(() => console.log("Share was successful."))
+          .catch((error) => console.log("Sharing failed", error));
+      } else {
+        console.log(`Your system doesn't support sharing.`);
+      }
+    },
+  },
+});
+</script>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,19 +1,45 @@
 <template>
-  <div class="grid">
-    <h1>Settings</h1>
-    <h2>Your goals</h2>
-    <GoalSettings />
+  <div class="grid big-gap">
+    <h1 style="margin-bottom: 0">Settings</h1>
+    <div class="grid">
+      <h2>Your goals</h2>
+      <GoalSettings />
+    </div>
+    <div class="grid">
+      <h2>Install the app</h2>
+      <InstallApp />
+    </div>
+    <div class="grid">
+      <h2>iOS Shortcut</h2>
+      <p>
+        The iOS Shortcut allows you to copy your nutritional intake from this
+        app directly into Health. Install the Shortcut on your iPhone to get
+        started.
+      </p>
+      <a
+        href="https://www.icloud.com/shortcuts/a0c928d5389c4fa991da591685e79796"
+        target="_blank"
+      >
+        Get the iOS Shortcut
+      </a>
+      <p>
+        When you've got the Shortcut installed, you can copy the data from the
+        <router-link to="/progress">Progress</router-link> page into Health.
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import GoalSettings from "@/components/GoalSettings.vue"; // @ is an alias to /src
+import InstallApp from "@/components/InstallApp.vue";
 
 export default defineComponent({
   name: "SettingsView",
   components: {
     GoalSettings,
+    InstallApp,
   },
 });
 </script>
