be2bba9f34efd40782c380a53b7f0b1ef9a5d293
Michal <mwargan@gmail.com>
2023-05-01 18:10:39 +0200

    refactor: location of food functions

--- src/components/FoodItemList.vue
+++ src/components/FoodItemList.vue
@@ -7,9 +7,11 @@
         :src="food.image_url"
         :name="food.name"
         :categories="getFoodCategoriesForFoodItem(food)"
-        :lastMonth="getLastMonthInARowFromFoodItem(food)"
-        :localName="getLocalName(food)"
-        :isNative="checkIsFoodNativeToCountry(food)"
+        :lastMonth="
+          getLastMonthInARowFromFoodItem(food, filters.countryCode ?? '')
+        "
+        :localName="getLocalName(food, filters.countryCode ?? '')"
+        :isNative="isNativeToCountry(food, filters.countryCode ?? '')"
         :calories="food.kcal"
         :carb="food.carbohydrate"
         :fat="food.fat"
@@ -41,16 +43,12 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, defineProps, defineExpose, toRaw } from "vue";
-import {
-  CategoryName,
-  FoodItem as FoodItemTs,
-  MonthName,
-} from "@/types/foodItem";
+import { computed, defineProps, defineExpose } from "vue";
+import { FoodItem as FoodItemTs } from "@/types/foodItem";
 import type { PropType } from "vue";
 import FoodItem from "@/components/FoodItem.vue"; // @ is an alias to /src
 import store from "@/store";
-import { consumedItem } from "@/types/consumedItem";
+import { useFood } from "@/composables/useFood";
 
 const props = defineProps({
   foods: {
@@ -89,220 +87,27 @@ const props = defineProps({
   },
 });
 
-const checkIsFoodNativeToCountry = (food: FoodItemTs): boolean | null => {
-  return (
-    food.food_regions?.find((foodRegion) => {
-      return (
-        foodRegion.region.country_code.toLowerCase() ===
-        props.filters.country?.toLowerCase()
-      );
-    })?.grows_in_region ?? null
-  );
-};
+const {
+  getFinalFood,
+  getFoodCategoriesForFoodItem,
+  getLastMonthInARowFromFoodItem,
+  getLocalName,
+  isNativeToCountry,
+  helpsReachGoals,
+  timesConsumedToday,
+  isDisliked,
+} = useFood();
 
-const getFoodCategoriesForFoodItem = (food: FoodItemTs): CategoryName[] => {
-  if (!food.categories) {
-    return [];
-  }
-  return food.categories.map((foodCategory) => {
-    return foodCategory.name;
-  });
-};
-
-const getLastMonthInARowFromFoodItem = (
-  food: FoodItemTs
-): MonthName | undefined => {
-  const months = food.food_regions
-    ?.find((foodRegion) => {
-      return (
-        foodRegion.region.country_code.toLowerCase() ===
-        props.filters.country?.toLowerCase()
-      );
-    })
-    ?.seasons.map((season) => {
-      return season.month_name;
-    });
-  if (!months) {
-    return;
-  }
-  return months[months.length - 1];
-};
-
-const getLocalName = (food: FoodItemTs): string => {
-  return (
-    food.food_regions?.find((foodRegion) => {
-      return (
-        foodRegion.region.country_code.toLowerCase() ===
-        props.filters.country?.toLowerCase()
-      );
-    })?.local_name ?? ""
-  );
-};
-
-const foodItemsThatHelpReachGoals = computed(
-  () => store.getters["foodItems/foodItemsThatHelpReachGoals"]
+const showableFoods = computed(() =>
+  !props.sort && !props.filters
+    ? props.foods
+    : getFinalFood(props.foods, props.filters, props.sort)
 );
-
-const helpsReachGoals = (id: number) => {
-  return (
-    foodItemsThatHelpReachGoals.value.findIndex(
-      (foodItem: FoodItemTs) => foodItem.id === id
-    ) !== -1
-  );
-};
-
-const consumedItemsToday = computed(
-  () => store.getters["consumedItems/allConsumedItemsToday"]
-);
-
-const timesConsumedToday = (id: number) => {
-  // Count amount of times this foodId appears in consumedItemsToday
-  return (
-    consumedItemsToday.value.filter(
-      (foodItem: consumedItem) => foodItem.food_id === id
-    ).length ?? 0
-  );
-};
 
 const isSignedUp = computed(() => store.getters["auth/isSignedUp"]);
 const supportsIndexedDB = store.getters["app/supportsIndexedDB"];
 
-const dislikedFoodItemIds = computed(
-  () => store.getters["consumedItems/dislikedFoodItemIds"]
-);
-
-const isDisliked = (id: number) => {
-  return dislikedFoodItemIds.value.includes(id);
-};
-
-const hasSeasonalInfo = (food: FoodItemTs) => {
-  return food.food_regions?.some((foodRegion) => {
-    return (
-      foodRegion.region.country_code.toLowerCase() ===
-      props.filters.country?.toLowerCase()
-    );
-  });
-};
-
-const foodIsInCountry = (food: FoodItemTs, countryCode: string) => {
-  return (
-    food.food_regions?.some((foodRegion) => {
-      return (
-        foodRegion.region.country_code.toLowerCase() ===
-        countryCode.toLowerCase()
-      );
-    }) ?? false
-  );
-};
-
-const foodIsInRegion = (food: FoodItemTs, regionName: string) => {
-  return (
-    food.food_regions?.some((foodRegion) => {
-      return foodRegion.region.name.toLowerCase() === regionName.toLowerCase();
-    }) ?? false
-  );
-};
-
-const isInSeasonForRegion = (
-  food: FoodItemTs,
-  countryCode: string,
-  regionName: string,
-  month: string
-) =>
-  food.food_regions?.findIndex(
-    (foodRegion) =>
-      foodRegion.region.name.toLowerCase() === regionName.toLowerCase() &&
-      foodRegion.region.country_code.toLowerCase() ===
-        countryCode.toLowerCase() &&
-      foodRegion.seasons.findIndex(
-        (season) => season.month_name.toLowerCase() === month.toLowerCase()
-      ) !== -1
-  ) !== -1;
-
-// Determine if the food item is in season based on the month in filters.month
-const isInSeason = (food: FoodItemTs) => {
-  if (!hasSeasonalInfo(food)) {
-    return false;
-  }
-
-  if (!props.filters.country) {
-    return false;
-  }
-
-  if (!foodIsInCountry(food, props.filters.country)) {
-    return false;
-  }
-
-  if (!props.filters.region) {
-    return false;
-  }
-
-  if (!foodIsInRegion(food, props.filters.region)) {
-    return false;
-  }
-
-  if (!props.filters.month) {
-    return false;
-  }
-
-  if (
-    !isInSeasonForRegion(
-      food,
-      props.filters.country,
-      props.filters.region,
-      props.filters.month
-    )
-  ) {
-    return false;
-  }
-
-  return true;
-};
-
-const showableFoods = computed(() =>
-  props.foods
-    .filter((food) => {
-      return (
-        (props.filters.hideDisliked ? !isDisliked(food.id) : true) &&
-        (props.filters.hideHelpsReachGoals
-          ? !helpsReachGoals(food.id)
-          : true) &&
-        (props.filters.showOnlyWithCaloricInfo
-          ? food.kcal && food.kcal > 0
-          : true) &&
-        (props.filters.showOnlyWithCountry && checkIsFoodNativeToCountry(food)
-          ? checkIsFoodNativeToCountry(food)
-          : true) &&
-        (props.filters.showOnlyInSeason ? isInSeason(food) : true)
-      );
-    })
-    .sort((a: FoodItemTs, b: FoodItemTs) => {
-      if (!props.sort) {
-        return 0;
-      }
-
-      const aValue = a[props.sort.by];
-      const bValue = b[props.sort.by];
-
-      // If the values don't exist, put them at the end of the list
-      if (aValue === undefined || bValue === undefined) {
-        return props.sort.direction === "asc" ? 1 : -1;
-      }
-
-      if (aValue === null || bValue === null) {
-        return props.sort.direction === "asc" ? 1 : -1;
-      }
-      if (aValue > bValue) {
-        return props.sort.direction === "asc" ? 1 : -1;
-      }
-      if (aValue < bValue) {
-        return props.sort.direction === "asc" ? -1 : 1;
-      }
-      return 0;
-    })
-);
-
-const countOfShowableFoods = computed(() => toRaw(showableFoods).value.length);
+const countOfShowableFoods = computed(() => showableFoods.value.length);
 
 defineExpose({
   countOfShowableFoods,
---
+++ src/composables/useFood.ts
@@ -0,0 +1,191 @@
+import { computed } from "vue";
+import {
+  CategoryName,
+  CountryCode,
+  FoodItem as FoodItemTs,
+  MonthName,
+} from "@/types/foodItem";
+import store from "@/store";
+import { consumedItem } from "@/types/consumedItem";
+
+export function useFood() {
+  const isNativeToCountry = (
+    food: FoodItemTs,
+    countryCode: CountryCode
+  ): boolean | null =>
+    food.food_regions?.find(
+      (foodRegion) =>
+        foodRegion.region.country_code.toLowerCase() ===
+        countryCode.toLowerCase()
+    )?.grows_in_region ?? null;
+
+  const getFoodCategoriesForFoodItem = (food: FoodItemTs): CategoryName[] => {
+    if (!food.categories) {
+      return [];
+    }
+    return food.categories.map((foodCategory) => {
+      return foodCategory.name;
+    });
+  };
+
+  const getLastMonthInARowFromFoodItem = (
+    food: FoodItemTs,
+    countryCode: CountryCode
+  ): MonthName | undefined => {
+    const months = food.food_regions
+      ?.find((foodRegion) => {
+        return (
+          foodRegion.region.country_code.toLowerCase() ===
+          countryCode.toLowerCase()
+        );
+      })
+      ?.seasons.map((season) => {
+        return season.month_name;
+      });
+    if (!months) {
+      return;
+    }
+    return months[months.length - 1];
+  };
+
+  const getLocalName = (food: FoodItemTs, countryCode: CountryCode): string => {
+    return (
+      food.food_regions?.find((foodRegion) => {
+        return (
+          foodRegion.region.country_code.toLowerCase() ===
+          countryCode.toLowerCase()
+        );
+      })?.local_name ?? ""
+    );
+  };
+
+  const foodItemsThatHelpReachGoals = computed(
+    () => store.getters["foodItems/foodItemsThatHelpReachGoals"]
+  );
+
+  const helpsReachGoals = (id: number) => {
+    return (
+      foodItemsThatHelpReachGoals.value.findIndex(
+        (foodItem: FoodItemTs) => foodItem.id === id
+      ) !== -1
+    );
+  };
+
+  const consumedItemsToday = computed(
+    () => store.getters["consumedItems/allConsumedItemsToday"]
+  );
+
+  const timesConsumedToday = (id: number) => {
+    // Count amount of times this foodId appears in consumedItemsToday
+    return (
+      consumedItemsToday.value.filter(
+        (foodItem: consumedItem) => foodItem.food_id === id
+      ).length ?? 0
+    );
+  };
+
+  const dislikedFoodItemIds = computed(
+    () => store.getters["consumedItems/dislikedFoodItemIds"]
+  );
+
+  const isDisliked = (id: number) => dislikedFoodItemIds.value.includes(id);
+
+  const isInSeasonForRegion = (
+    food: FoodItemTs,
+    countryCode: string,
+    regionName: string,
+    month: string
+  ) =>
+    food.food_regions &&
+    food.food_regions.findIndex(
+      (foodRegion) =>
+        foodRegion.region.country_code.toLowerCase() ===
+          countryCode.toLowerCase() &&
+        foodRegion.region.name.toLowerCase() === regionName.toLowerCase() &&
+        foodRegion.seasons.findIndex(
+          (season) => season.month_name.toLowerCase() === month.toLowerCase()
+        ) !== -1
+    ) !== -1;
+
+  const getFilteredFood = (
+    foods: FoodItemTs[],
+    filters: Record<string, any>
+  ): FoodItemTs[] => {
+    return foods.filter(
+      (food) =>
+        (filters.hideDisliked ? !isDisliked(food.id) : true) &&
+        (filters.hideHelpsReachGoals ? !helpsReachGoals(food.id) : true) &&
+        (filters.showOnlyWithCaloricInfo ? food.kcal && food.kcal > 0 : true) &&
+        (filters.showOnlyWithCountry && filters.country
+          ? isNativeToCountry(food, filters.country)
+          : true) &&
+        (filters.showOnlyInSeason
+          ? isInSeasonForRegion(
+              food,
+              filters.country ?? "",
+              filters.region ?? "",
+              filters.month ?? ""
+            )
+          : true)
+    );
+  };
+
+  const getSortedFood = (
+    foods: FoodItemTs[],
+    sort:
+      | {
+          by: keyof FoodItemTs;
+          direction: "asc" | "desc";
+        }
+      | false
+  ): FoodItemTs[] => {
+    return foods.sort((a: FoodItemTs, b: FoodItemTs) => {
+      if (!sort) {
+        return 0;
+      }
+
+      const aValue = a[sort.by];
+      const bValue = b[sort.by];
+
+      // If the values don't exist, put them at the end of the list
+      if (aValue === undefined || bValue === undefined) {
+        return sort.direction === "asc" ? 1 : -1;
+      }
+
+      if (aValue === null || bValue === null) {
+        return sort.direction === "asc" ? 1 : -1;
+      }
+      if (aValue > bValue) {
+        return sort.direction === "asc" ? 1 : -1;
+      }
+      if (aValue < bValue) {
+        return sort.direction === "asc" ? -1 : 1;
+      }
+      return 0;
+    });
+  };
+
+  const getFinalFood = (
+    foods: FoodItemTs[],
+    filters: Record<string, any>,
+    sort:
+      | {
+          by: keyof FoodItemTs;
+          direction: "asc" | "desc";
+        }
+      | false
+  ): FoodItemTs[] => {
+    return getSortedFood(getFilteredFood(foods, filters), sort);
+  };
+
+  return {
+    getFinalFood,
+    getFoodCategoriesForFoodItem,
+    getLastMonthInARowFromFoodItem,
+    getLocalName,
+    timesConsumedToday,
+    isNativeToCountry,
+    helpsReachGoals,
+    isDisliked,
+  };
+}
